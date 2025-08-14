c7eb47be34363429f5e897666d22f3b1edffc4a8
sschanbkhn <sschanbkhn@gmail.com>
2025-08-14 13:53:39 +0700

    Commit đầu tiên: r005 sleeping cell 14082025_1153

--- src/components/SNOC/hooks/useScheduleWebSocket.js
+++ src/components/SNOC/hooks/useScheduleWebSocket.js
@@ -2,6 +2,27 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { updateLastRunAt } from "../redux/Healthcheck/healthcheckSlice";
 
+/*
+const useScheduleWebSocket = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("⚠️ WebSocket tạm thời được disable");
+
+    // Tạm thời không làm gì, chỉ return cleanup function rỗng
+    return () => {
+      // Cleanup rỗng
+    };
+  }, [dispatch]);
+
+  // Return empty object hoặc null
+  return {};
+};
+
+export default useScheduleWebSocket;
+
+*/
+
 const useScheduleWebSocket = () => {
   const dispatch = useDispatch();
 
@@ -18,7 +39,6 @@ const useScheduleWebSocket = () => {
             name: data.schedule_name,
             status: data.status,
             last_run_at: data.last_run_at,
-            
           })
         );
       } else {
