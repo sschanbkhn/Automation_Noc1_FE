bb614a4b01c82ca6d4bf046451f57ac7d6a74b75
Truantneet <dothanhhai@vnpt.vn>
2025-09-09 14:17:44 +0700

    Add files via upload

---
+++ src/components/ANM2/UC5/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
+const Anm_uc1 = () => {
+  return (
+    <div className="p-3">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Anm_uc1;
---
+++ src/components/ANM3/UC1/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
+const Anm2_uc1 = () => {
+  return (
+    <div className="p-3">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Anm2_uc1;
